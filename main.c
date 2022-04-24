4e0f17d399f36ef2e362c0dd85919f0f9c2b1072
Johannes <johannes.haapanen@gmail.com>
2022-04-24 11:55:07 +0300

    Added built in functions back

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "command.h"
 
 
-
+// Prompt wish>
 char *prompt() {
     size_t len = 0;
     char *line = NULL;
@@ -27,19 +27,53 @@ char *prompt() {
     }
 }
 
+// Built in function for cd
+void b_cd(char *dest){
+  if (chdir(dest) != 0) {
+        write(STDERR_FILENO, ERRMSG, strlen(ERRMSG));
+    }
+}
+
+// Built in function for exit
+void b_exit(char *dest){
+  exit(0);
+}
+
+// Checks if there is built in command and executes it if found
+bool run_built_in(Command *cmd){
+
+  // List of function names
+  char *func_names[] = {"exit", "cd"};
+
+  // Number of built in commands
+  int num_func_names = sizeof(func_names) / sizeof(char  *);
+
+  // Function pointes to built in command functions
+  void (*func_p[])(char *) = {&b_cd, &b_exit};
+
+    // Checks if there is built in command and runs it
+    for (int i=0; i<num_func_names; i++){
+      if (strcmp(cmd->command, func_names[i]) == 0){
+        (*func_p)(cmd->args[1]);
+        return true;
+      }
+    }
+    // Returns false if there was no built in command
+    return false;
+}
+
 int main(int argc, char **argv) {
   char *line = NULL;
   Command *cmd = NULL;
   Path *path = NULL;
 
   // Built in function names
-  char *func_names[] = {"exit"};
-  int num_func_names = sizeof(func_names) / sizeof(char  *);
-
 
   path = Path__init(path);
  
   while (true) {
+
+    //(*func_p[0])("C");
 
     // wish> prompt -> returns the raw input line
     line = prompt();
@@ -47,10 +81,14 @@ int main(int argc, char **argv) {
     // Initilizes the cmd structure
     cmd = Command__init(cmd);
     Command__build(cmd, line);
+
+    if (run_built_in(cmd)) {
+      continue;
+    }
+
+    // Executes a program from given path
+    // TODO Use Path object
     Command__execute(cmd, "/bin/");
-
-    //TODO Built ins
-
   }
 }
 
